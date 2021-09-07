a5c693e065cd1516b417e001f6ffdf2511b83dfe
Dakota <d-pfeif@outlook.com>
2021-09-07 09:12:05 -0700

    folder aliases

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,17 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        alias: {
+          "@assets": "./src/assets",
+          "@components": "./src/components",
+          "@containers": "./src/containers",
+          "@styles": "./src/styles",
+        },
+      }
+    ]
+  ]
 };
